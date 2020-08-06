596f4bf5da300ef4f1c936aa4a2a5499d79497e7
johannes <jmuljana@gmail.com>
2020-08-06 15:16:01 +1000

    placed TouchableOpacity button inside a View

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React , {Component} from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button, TouchableOpacity } from 'react-native';
 
 import {Item} from './components/Item';
 
 export default class App extends Component {
   state = {
     expenseAmount : 0,
-    expenseCategory: ''
+    expenseCategory: '',
+    updating: false,
   }
-  listData = [
-    
-  ]
+  listData = []
+
   render() {
     return (
-      <SafeAreaView>
+      <SafeAreaView >
+        <View style={styles.main}>
         <Text>Add your expense</Text>
         <TextInput 
           style={styles.input}
@@ -26,12 +27,20 @@ export default class App extends Component {
           placeholder="category"
           onChangeText={ text => this.setState({ expenseCategory: text }) }
         />
-        <Button title="Add" onPress={this.addItem()} />
+        </View>
+        {/* wrap the button in view */}
+        <View>
+          <TouchableOpacity  style={styles.button} onPress= {this.addItem}>
+            <Text style={styles.buttonText} >Add</Text>
+          </TouchableOpacity>
+        </View>
+        
+        
         <FlatList 
-          data={this.listData}
-          renderItem={this.renderList}
-          keyExtractor={ expense => expense.id }
-          extraData={this.state.expenseAmount}
+          data = {this.listData}
+          renderItem = { this.renderList }
+          keyExtractor = { item => item.id }
+          extraData = {this.state.expenseAmount}
         />
       </SafeAreaView>
     )
@@ -40,20 +49,27 @@ export default class App extends Component {
     <Item amount={item.amount} category={item.category} />
   )
   addItem = () => {
-    if( isNaN(this.state.expenseAmount) || this.state.expenseAmount == 0 ) {
+    if( 
+      isNaN(this.state.expenseAmount) || 
+      this.state.expenseAmount == 0 || 
+      this.state.expenseCategory == '' ) {
       return;
     }
     let itemId = new Date().getTime().toString()
-    let item = {
+    let listItem = {
       id: itemId,
       amount: this.state.expenseAmount,
       category: this.state.expenseCategory
     }
-    this.listData.push(item)
+    this.listData.push(listItem)
+    console.log('adding')
+    // this.setState({updating: true})
     this.setState({expenseAmount:0})
   }
 }
-
+const colors = {
+  primary : 'hsla(330, 38%, 65%, 1)'
+}
 const styles = StyleSheet.create({
   main: {
     paddingHorizontal: 10,
@@ -64,5 +80,13 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     marginVertical: 15
+  },
+  button: {
+    padding: 15,
+    backgroundColor: colors.primary
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
   }
 })
