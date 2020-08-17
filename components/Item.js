231c3e48d9e6f8ecce0cf67d71f4f08db3d783b5
johannes <jmuljana@gmail.com>
2020-08-17 18:04:05 +1000

    added delete icon

--- components/Item.js
+++ components/Item.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Text,View,StyleSheet } from 'react-native';
+import { Text,View,TouchableOpacity,Image } from 'react-native';
 
 import {itemStyles} from '../styles/Item';
 
 export const Item = ( props ) => {
   return(
     <View style={itemStyles.item}>
-      <Text style={itemStyles.text}>{props.category}</Text>
-      <Text style={itemStyles.text}>{props.amount}</Text>
+      <View style={itemStyles.row}>
+        <Text style={itemStyles.text}>{props.category}</Text>
+        <Text style={itemStyles.text}>{props.amount}</Text>
+      </View>
+      <TouchableOpacity>
+        <Image style={itemStyles.icon} source={require('../assets/trash-alt-solid.png')} />
+      </TouchableOpacity>
     </View>
   )
 }
--- styles/Item.js
+++ styles/Item.js
@@ -6,10 +6,20 @@ export const itemStyles = StyleSheet.create({
     padding: 10,
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   text: {
     fontSize: 16,
     color: 'black'
-  }
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingRight: 10,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
 })
